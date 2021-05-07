e61293efe2f1b0a9460052dd88a1335a6302cad3
kuribo2110 <kuribo2110@gmail.com>
2021-05-07 12:41:17 +0900

    add Header

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
+  <Header/>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
+import Header from './components/Header.vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
+    Header,
     HelloWorld
   }
 }
@@ -21,6 +24,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+    <nav class="navbar navbar-dark bg-dark">
+      <div>
+        <a class="navbar-brand font-weight-bold" href="#">JAPAN-PR</a>
+      </div>
+      <div class="links">
+        <a class="navbar-brand" href="#">Food</a>
+        <a class="navbar-brand" href="#">Culture</a>
+        <a class="navbar-brand" href="#">About Us</a>
+        <a class="navbar-brand" href="#">Team</a>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+@media screen and (max-width: 767px) {
+  .links {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+  }
+  .navbar-brand {
+    margin: 0;
+  }
+}
+@media screen and (min-width: 768px) {
+  .navbar-brand:not(:last-child) {
+    margin-right: 2rem;
+  }
+}
+</style>
