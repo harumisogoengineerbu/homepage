189637e2ad184260036e5b5841145f4ad25a5cc5
Administrator <administrator@AdminisatorsAir.fritz.box>
2021-05-08 15:08:53 +0200

    add section startsite and foods html

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,85 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="section section-startsite">
+    <div class="main-section">
+      <div class="left-column">
+        <h1>O MO TE NA SHI</h1>
+        <p>Omotenashi is to welcome guests with a heart that doesn't change depending on something</p>
+        <table>
+          <tr>
+            <th>our contents</th>
+          </tr>
+          <tr>
+            <td>
+              <a href="#foods">Japnese Foods</a>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <a href="#cultures">Japanese Cultures</a>
+            </td>
+          </tr>
+        </table>
+      </div>
+      <div class="right-column">
+        <img src="./assets/kyoto-night.jpg" height="400" />
+      </div>
+    </div>
+  </div>
+
+  <div class="section-black"></div>
+  
+  <div id="foods" class="section section-foods">
+    <div class="main-section">
+      <div class="section-title">
+        <h1>Japanese Foods</h1>
+      </div>
+      <div class="first-line">
+        <div class="left-column">
+          <img src="./assets/sushi.jpg" height="400" />
+        </div>
+        <div class="right-column">
+          <h1>Sushi</h1>
+          <p></p>
+        </div>
+      </div>
+      <div class="second-line">
+        <div class="left-column">
+          <h1>Okonomiyaki</h1>
+          <p></p>
+        </div>
+        <div class="right-column">
+          <img src="./assets/okonomiyaki.jpg" height="400" />
+        </div>
+      </div>
+      <div class="third-line">
+        <div class="left-column">
+          <img src="./assets/ramen.jpg" height="400" />
+        </div>
+        <div class="right-column">
+          <h1>Ramen</h1>
+          <p></p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="section-black"></div>
+
+  <div id="cultures" class="section section-cultures">
+    <div class="main-section"></div>
+      <div class="section-title">
+        <h1>Japanese Cultures</h1>
+      </div>
+      <div class="left-column"></div>
+      <div class="right-column"></div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+<script></script>
 
 <style>
+@import "./css/startsite.css";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.config.productionTip = false;
+app.mount("#app");
