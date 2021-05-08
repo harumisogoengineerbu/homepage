de1b5d93709bff3bae97f5ef69c0126b9cf5e017
kuribo2110 <kuribo2110@gmail.com>
2021-05-08 15:06:19 +0900

    cahnge Bootstrap to v5

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,16 @@
 <template>
   <header>
     <nav class="navbar navbar-dark bg-dark">
-      <div>
-        <a class="navbar-brand font-weight-bold" href="#">JAPAN-PR</a>
-      </div>
-      <div class="links">
-        <a class="navbar-brand" href="#">Food</a>
-        <a class="navbar-brand" href="#">Culture</a>
-        <a class="navbar-brand" href="#">About Us</a>
-        <a class="navbar-brand" href="#">Team</a>
+      <div class="container-fluid">
+        <div>
+          <a class="navbar-brand fw-bold" href="#">JAPAN-PR</a>
+        </div>
+        <div class="links">
+          <a class="navbar-brand" href="#">Food</a>
+          <a class="navbar-brand" href="#">Culture</a>
+          <a class="navbar-brand" href="#">About Us</a>
+          <a class="navbar-brand" href="#">Team</a>
+        </div>
       </div>
     </nav>
   </header>
@@ -26,6 +28,9 @@
   }
 }
 @media screen and (min-width: 768px) {
+  .navbar {
+    padding: 1rem 0;
+  }
   .navbar-brand:not(:last-child) {
     margin-right: 2rem;
   }
