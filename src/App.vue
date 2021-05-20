91aff38f713ca57f26f201a5b9b9ae64226ff41d
kuribo2110 <kuribo2110@gmail.com>
2021-05-20 23:15:51 +0900

    add animation

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,56 @@
 <template>
-  <Header/>
-  <Startsite/>
-  <Introduce/>
-  <Black/>
-  <Foods/>
-  <Black/>
-  <Cultures/>
-  <Footer/>
+  <div class='fadein'>
+    <Header />
+    <Startsite />
+    <Introduce />
+    <Black />
+    <Foods />
+    <Black />
+    <Cultures />
+    <Footer />
+  </div>
 </template>
 
 <script>
-  import Header from './components/Header.vue'
-  import Startsite from './components/Startsite.vue'
-  import Black from './components/Black.vue'
-  import Introduce from './components/Introduce.vue'
-  import Foods from './components/Foods.vue'
-  import Cultures from './components/Cultures.vue'
-  import Footer from './components/Footer.vue'
+import Header from './components/Header.vue'
+import Startsite from './components/Startsite.vue'
+import Black from './components/Black.vue'
+import Introduce from './components/Introduce.vue'
+import Foods from './components/Foods.vue'
+import Cultures from './components/Cultures.vue'
+import Footer from './components/Footer.vue'
 
-  export default {
-    name: 'App',
-    components: {
-      Header,
-      Startsite,
-      Black,
-      Introduce,
-      Foods,
-      Cultures,
-      Footer
-    }
-  } 
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Startsite,
+    Black,
+    Introduce,
+    Foods,
+    Cultures,
+    Footer
+  }
+}
 </script>
 
 <style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.fadein {
+  animation: fadein 1s;
+}
+@keyframes fadein {
+  0% {
+    opacity: 0;
   }
+  100% {
+    opacity: 1;
+  }
+}
 </style>
