d8d24c13f2c5c7fb9fd613ab731c9105428fd90e
Administrator <administrator@AdminisatorsAir.fritz.box>
2021-05-30 08:08:02 +0200

    add Introduce.vue

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
